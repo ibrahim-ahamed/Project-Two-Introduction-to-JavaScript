67f3ff5baba29f1a7c384024a87ef2f4379a6a16
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-27 21:13:19 +0600

    Number js

---
+++ number.js
@@ -0,0 +1,22 @@
+var orangePrice = 20; // number type
+//var applePrice = "30"; // string type
+var chocolate = .5; // number type
+var googlePrice = parseInt("40"); // convert string to number
+var facebookPrice = parseFloat("50.5"); // convert string to float number
+
+
+
+console.log(orangePrice + chocolate + googlePrice + facebookPrice); // addition of numbers
+console.log(typeof orangePrice);
+// console.log(typeof applePrice);
+console.log(typeof chocolate);
+console.log(typeof googlePrice);
+console.log(typeof facebookPrice);
+
+
+
+var first = 0.6;
+var second = 0.7;
+var total = first + second;
+console.log(total); // 1.2999999999999998
+console.log(total.toFixed(5)); // 1.30
