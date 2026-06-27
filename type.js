172896ad82734c439ee7eccb7ff4b4444eea3727
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-27 10:00:10 +0600

    Type Of variable

---
+++ type.js
@@ -0,0 +1,19 @@
+// Type Of varibale= String
+var fullName = 'Ibrahim Ahmed';
+var address = 'Dhaka, Bangladesh';
+var country = 'Bangladesh';
+
+console.log(typeof fullName);
+
+//  Type Of varibale= Number
+var age = 25;
+var phoneNumber = 01728336395;
+var price = 500;
+
+console.log(typeof age);
+
+// type Of varibale= Boolean
+var isMarried = false;
+var isSingle = true;
+
+console.log(typeof isMarried);
