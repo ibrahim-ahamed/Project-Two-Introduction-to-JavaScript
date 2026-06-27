e4b7c80cc9b3bb06299ee11b7092fb5e1dd7e857
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-27 21:37:22 +0600

    Mixed opi

---
+++ mixopi.js
@@ -0,0 +1,9 @@
+var first = 'mobarok';
+var second = 'tobarok';
+
+var fullName = first + second;
+// console.log(fullName);
+console.log('40' + 20); // 4020
+console.log('40' - 20); // 20
+console.log('40' * 20); // 800
+console.log('40' / 20); // 2
