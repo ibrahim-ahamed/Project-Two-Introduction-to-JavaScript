a336fe600285e838829892691e44003790a4474e
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-27 09:17:21 +0600

    Math

---
+++ math.js
@@ -0,0 +1,16 @@
+/* 
+***************************************************
+*            MATH NEEDED FOR PROGRAMING
+* *************************************************
+* 1. Number: +Ve, Ve-, 0 [ 50, -9, 0]
+* 2. integer: 2, 88, [odd, even]: 2, 4, 897
+* 3. Float: 5.6, 2.2
+* 4. operation: +, -, *, /
+* 5. More  Operation: %, []
+* 6. percentage: 5%, 50 er 10% koto?
+* 7. Sum: 14, 18, 16,
+* 8. avg: 14, 18, 16
+* 9. Max/Min: 14, 18, 16
+* 10. Series: 
+* 11. Area: Triangle
+*/
