fd4a4a29719601ecb2ce6ce82fbb8bcf8040bb27
Ibrahim Ahmed <mdibrahimahmed1231@gmail.com>
2026-06-27 21:36:28 +0600

    Short Hand

---
+++ shorthand.js
@@ -0,0 +1,20 @@
+var price = 20;
+// price = price + 10; // price = price + 10
+price += 10; // shorthand operator
+
+console.log(price);
+
+var fridge = 35000;
+// fridge = fridge - 5000; // fridge = fridge - 5000
+fridge -= 5000; // shorthand operator   
+// console.log(fridge);
+
+// multiply
+var orangePrice = 20;
+// orangePrice = orangePrice * 2; // orangePrice = orangePrice * 2
+orangePrice *= 2; // shorthand operator
+// console.log(orangePrice);
+
+// \\
+
+
